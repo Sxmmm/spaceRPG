c54ec9e673a929bcb686908a646feb4f0d5c353b
Sam Baker <s1704633@connect.glos.ac.uk>
2018-11-20 02:27:46 +0000

    Base stuff added
    .Movement
    .Sorting order
    .Camera follow

---
+++ Assets/Scripts/PlayerControlsScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControlsScript : MonoBehaviour {
+
+    public float moveSpeed = 3f;
+    int direction = 1;
+    float xpos;
+
+
+    void Start ()
+    {
+        xpos = transform.position.x;
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.up * Time.deltaTime * Input.GetAxis("Vertical") * moveSpeed);
+        transform.Translate(Vector3.right * Time.deltaTime * Input.GetAxis("Horizontal") * moveSpeed);
+
+        if (transform.position.x < xpos)
+        {
+            if (direction == 1)
+            {
+                transform.localScale = new Vector2(-8, transform.localScale.y);
+                direction = -1;
+            }
+        }
+        else
+        {
+            if (direction == -1)
+            {
+                transform.localScale = new Vector2(8, transform.localScale.y);
+                direction = 1;
+            }
+        }
+        xpos = transform.position.x;
+    }
+}
---
+++ Assets/Scripts/SpriteOrderScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteOrderScript : MonoBehaviour {
+
+    void Update()
+    {
+        SpriteRenderer[] renderers = FindObjectsOfType<SpriteRenderer>();
+
+        foreach(SpriteRenderer renderer in renderers)
+        {
+            renderer.sortingOrder = (int)(renderer.transform.position.y * -100);
+        }
+    }
+
+}
