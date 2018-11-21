02015f426a77273d9cfea790e05018ac876d3cb2
JackTilling <43538414+JackTilling@users.noreply.github.com>
2018-11-21 11:39:55 +0000

    Health Bar + System Working

---
+++ Assets/Scripts/PlayerCombatScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombatScript : MonoBehaviour {
+
+    [SerializeField]
+    private int maxHealth = 100;
+
+    [SerializeField]
+    private int currentHealth;
+
+    [SerializeField]
+    private GameObject uiObject;
+
+    [SerializeField]
+    private UIManagerScript healthBar;
+
+    private void OnEnable() {
+        currentHealth = maxHealth;
+        uiObject = GameObject.Find("UI");
+        healthBar = uiObject.GetComponent<UIManagerScript>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space) & currentHealth > 0) {
+            ChangeHealth(-10);
+        }
+        if (Input.GetKeyDown(KeyCode.R)) {
+            int toMaxHealth = maxHealth - currentHealth;
+            ChangeHealth(toMaxHealth);
+        }
+    }
+
+    private void ChangeHealth(int hpChange) {
+        currentHealth += hpChange;
+
+        // Must convert to float in order to get result out of 1 (percentage for fill amount)
+        float currentHealthPer = (float)currentHealth / (float)maxHealth;
+
+        healthBar.HandleHealthChanged(currentHealthPer);
+    }
+}
---
+++ Assets/Scripts/UIManagerScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManagerScript : MonoBehaviour {
+
+    [SerializeField]
+    private float updateSpeedSeconds = 0.5f;
+
+    [SerializeField]
+    private Image foregroundImg;
+
+    public void HandleHealthChanged(float per) {
+        StartCoroutine(ChangeToPer(per));
+    }
+
+    private IEnumerator ChangeToPer(float per) {
+        float preChangePer = foregroundImg.fillAmount;
+        float elapsed = 0f;
+
+        while (elapsed < updateSpeedSeconds) {
+            elapsed += Time.deltaTime;
+            foregroundImg.fillAmount = Mathf.Lerp(preChangePer, per, elapsed / updateSpeedSeconds);
+            yield return null;
+        }
+        foregroundImg.fillAmount = per;
+    }
+}
